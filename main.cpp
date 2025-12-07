bf7cf6cb6f5482222f9d9b52896ec9e242ae83b0
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-12-06 20:22:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,26 +14,24 @@ using namespace winrt::Windows::UI::Notifications;
 
 extern "C" {
     __declspec(dllexport) BOOL WINAPI ShowToast(const char* title, const char* body) {
-        init_apartment();
-        
-        auto doc = XmlDocument();
-        std::wstring xml = L"<toast>"
-            L"<visual>"
-            L"  <binding template=\"ToastGeneric\">"
-            L"    <text>" + winrt::to_hstring(title) + L"</text>"
-            L"    <text>" + winrt::to_hstring(body) + L"</text>"
-            L"  </binding>"
-            L"</visual>"
-            L"<actions>"
-            L"  <action content=\"OK\" arguments=\"action=ok\"/>"
-            L"</actions>"
-            L"</toast>";
-        
-        doc.LoadXml(xml);
-        ToastNotification toast(doc);
-        auto notifier = ToastNotificationManager::CreateToastNotifier();
-        notifier.Show(toast);
-        return TRUE;
+        try {
+            init_apartment();
+            
+            auto doc = XmlDocument();
+            hstring xml_content = L"<toast><visual><binding template=\"ToastGeneric\">"
+                                L"<text fill=\"{textPrimary}\">" + to_hstring(title) + L"</text>"
+                                L"<text fill=\"{textSecondary}\">" + to_hstring(body) + L"</text>"
+                                L"</binding></visual>"
+                                L"<actions><action content=\"OK\" arguments=\"action=ok\"/></actions></toast>";
+            
+            doc.LoadXml(xml_content);
+            ToastNotification toast(doc);
+            auto notifier = ToastNotificationManager::CreateToastNotifier();
+            notifier.Show(toast);
+            return TRUE;
+        } catch (...) {
+            return FALSE;
+        }
     }
     
     __declspec(dllexport) BOOL WINAPI InitToast() {
